12254b46ea954e30289a837147b2486c4fb20fa5
Arthi Ramesh <arthirvmhss@gmail.com>
2024-06-29 14:09:31 +0530

    both changes in projets link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ export default function Projects() {
         ]
     }
 
-    return <section id='projects' className="flex  flex-col py-20 px-5 justify-center bg-primary text-white">
+    return <section id='Projects' className="flex  flex-col py-20 px-5 justify-center bg-primary text-white">
         <div className="w-full">
             <div className="flex flex-col px-10 py-5">
                 <h1 className="text-4xl border-b-4 border-secondary mb-5 w-[150px] font-bold">Projects</h1>
